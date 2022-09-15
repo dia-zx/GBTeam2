6a90e83119b9b046f2d23b52b3ee3ff63895c59a
dia_zx <dia_zx@mail.ru>
2022-09-15 17:26:44 +0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+# GBTeam2
+# Групповая работа по созданию игры "крестики - нолики" с использованием telegram - bot
+# Точка входа
+# python -m venv .moduls
+# PowerShel:    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+# PowerShel:    Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser
+
+import controller
+
+controller.start()
